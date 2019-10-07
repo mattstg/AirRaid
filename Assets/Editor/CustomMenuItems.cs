8424b5a8ffa3b17110f6dc83bc3aecb4939338d4
Matt <mattstger@gmail.com>
2019-10-06 20:30:07 -0400

    Initial Push
    Git setup
    Asset Importation
    City Construction, Colliders & Layers setup
    Navmesh and obstacles setup

---
+++ Assets/Editor/CustomMenuItems.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomMenuItems : MonoBehaviour
+{
+    public static readonly HashSet<string> destructibleThings = new HashSet<string>() { "Bldg", "Tower", "Wall", "Prop" };
+
+
+    //Tool for setting up all the custom colliders needed for the imported assest
+    [MenuItem("Custom Menu/Setup Colliders")]
+    public static void SetupSceneColliders()
+    {
+        Transform selectedItem = Selection.activeTransform; //Get the clicked on transform
+        if (!selectedItem)
+        {
+            Debug.LogError("error, nothing selected");
+            return; //quit function
+        }
+
+
+
+    }
+}
