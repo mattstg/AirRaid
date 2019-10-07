713df4cf756c8b3d04369b685f04e5200a759c57
Matt <mattstger@gmail.com>
2019-10-06 21:05:03 -0400

    Player Prefab, Layers, Tags
    Added Layers and tags for: Wall,Tower, Building, Rocket, Player.
    Made player prefab, with turrets, rocket spawn spots and empty
    playerController script with links to those.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform[] rocketSpots;
+    public Transform[] gunTurretSpots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
