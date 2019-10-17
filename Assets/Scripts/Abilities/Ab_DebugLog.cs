6615acea37ca5e62fbe8f1c5ac774a2fa4667830
gosfi <44752770+gosfi@users.noreply.github.com>
2019-10-17 09:32:51 -0400

    creation fighter enemy script

---
+++ Assets/Scripts/Abilities/Ab_DebugLog.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ab_DebugLog : Ability
+{
+
+    public Ab_DebugLog(PlayerController _pc) : base(_pc)
+    {
+
+        stats = new AbilityStats(this, Abilities.Turrets, UpdateType.Update, .2f, 5f);
+    }
+    // Start is called before the first frame update
+    public override void AbilityPressed()
+    {
+
+        base.AbilityPressed();
+    }
+    public override void AbilityHeld()
+    {
+        UseAbility();
+        base.AbilityHeld();
+    }
+    public override bool UseAbility()
+    {
+        if (base.UseAbility())
+        {
+            Debug.Log("that's pretty cool!");
+            return true;
+        }
+
+        return false;
+    }
+    public override void AbilityRelease()
+    {
+        base.AbilityRelease();
+    }
+
+    public override void AbilityUpdate()
+    {
+        base.AbilityUpdate();
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public enum Abilities { Turrets, Rocket }
+public enum Abilities { Turrets, Rocket,WriteStuff }
 public enum BodyPart { BodyPart_Turret, BodyPart_WingSlots, BodyPart_BombBay, BodyPart_FrontCannon }  //These enums tags must EXCATLY match the tag names
 public class PlayerController : MonoBehaviour, IHittable
 {
@@ -23,12 +23,14 @@ public class PlayerController : MonoBehaviour, IHittable
     {
         //Create stats, add two starter abilities
         abilityManager = new AbilityManager(this);
-        abilityManager.AddAbilities(new Ab_MachineGun(this), 0); //Not the best way of adding an ability, it's a little unstable since it's not coupled with the inputSystem (for key pressing purposes)
+        abilityManager.AddAbilities(new Ab_MachineGun(this), 0);
+        abilityManager.AddAbilities(new Ab_DebugLog(this),1);//Not the best way of adding an ability, it's a little unstable since it's not coupled with the inputSystem (for key pressing purposes)
         //but it's important that I test now that my ability system is all in place.
 
         stats = new PlayerStats(this);
         stats.abilities.Add(Abilities.Turrets);
         stats.abilities.Add(Abilities.Rocket);
+        stats.abilities.Add(Abilities.WriteStuff);
 
         rb = GetComponent<Rigidbody>();
         rb.useGravity = false; //using custom gravity
