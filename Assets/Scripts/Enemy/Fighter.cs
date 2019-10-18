87344bb5c3145450e98331445bcd7b62f3f22350
gosfi <44752770+gosfi@users.noreply.github.com>
2019-10-18 12:09:28 -0400

    gtv

--- Assets/Scripts/Enemy/Fighter.cs
+++ Assets/Scripts/Enemy/Fighter.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fighter : MobileEnemy
+public class Fighter : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+
+    Vector3 dir;
+    GameObject player;
+    float timer;
+    public float speed;
+    [SerializeField] Rigidbody rb;
+    // [SerializeField] GameObject player;
+
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void FixedUpdate()
+    {
+        transform.LookAt(player.transform);
+
+        rb.AddForce(transform.forward * speed * Time.fixedDeltaTime, ForceMode.Acceleration);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EnemyType { Egg, EggSpitter, AATurret, Crawler}
+public enum EnemyType { Egg, EggSpitter, AATurret, Crawler,Fighter}
 public class EnemyManager
 {
     #region Singleton
