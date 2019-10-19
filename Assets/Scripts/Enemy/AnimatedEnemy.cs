ad8fda268e154574dce8bcc3aec798f8aef95116
zammorrak <46572000+zammorrak@users.noreply.github.com>
2019-10-19 09:35:19 -0400

    fix merge

--- Assets/Scripts/Enemy/AnimatedEnemy.cs
+++ Assets/Scripts/Enemy/AnimatedEnemy.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 
 public class AnimatedEnemy : MobileEnemy
 {
-<<<<<<< HEAD
-    Animator anim;
+
     Vector3 targetBuilding;
-=======
 
     protected Animator anim;
     protected float globalCoolDown; 
@@ -17,7 +15,6 @@ public class AnimatedEnemy : MobileEnemy
     protected float detectionRadius;
     protected Vector2 attackRadius; // Could be set in Initialize, by cycling through the abilities to get the min and max range overall
     protected AnimatorOverrideController animatorOverride;
->>>>>>> 4b48f3f2ea684118ff94e451ab062684b4e23b7c
     public override void Initialize(float startingEnergy)
     {
         base.Initialize(startingEnergy);
@@ -40,22 +37,16 @@ public class AnimatedEnemy : MobileEnemy
 
     public void DetectEnemyNearby() //Unsafe Function
     {
-<<<<<<< HEAD
-        int radius = 1;
+
         float buildingDistance = 0;
         float defenderDistance = 0;
         float distance;
         Vector3 positionBuilding = Vector3.zero;
-=======
+
         int radius = 1; // Use the detectionRadius variable instead
-        do
-        {
-            Collider[] collid = Physics.OverlapSphere(transform.position, radius, LayerMask.GetMask(""));
-        } while (true); // This will Crash!!!!
->>>>>>> 4b48f3f2ea684118ff94e451ab062684b4e23b7c
         
-        Collider[] collid = Physics.OverlapSphere(transform.position, radius, LayerMask.GetMask("Wall", "Building"));
-        if(collid != null)
+        Collider[] collid = Physics.OverlapSphere(transform.position, 10, LayerMask.GetMask("Wall", "Building"));
+        if(collid.Length != 0)
         {
             buildingDistance = Vector3.Distance(transform.position, collid[0].transform.position);
             foreach(Collider entity in collid)
