3111467d4d29765b5df1856c3c4cb1a3cb3e8c7d
Ahmad Sbeiti <ahmadsbeiti@outlook.com>
2019-10-20 18:28:41 -0400

    merge fix

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -9,23 +9,23 @@ public class InventoryManager : IManagable {
     public static InventoryManager Instance { get { return instance ?? (instance = new InventoryManager()); } }
     #endregion
 
-    public static Dictionary<string, Item> itemList;
+    public Dictionary<string, Item> itemList;
 
     public void Initialize() {
         itemList = new Dictionary<string, Item>();
     }
 
-    public void PostInitialize() {
-    }
+    public void PostInitialize() { }
 
-    public void PhysicsRefresh() {
-    }
+    public void PhysicsRefresh() { }
 
-    public void Refresh() {
-    }
+    public void Refresh() { }
 
-    //get slot from next available dictionairy item
     public void AddItem(int slot, Item item) {
         itemList.Add("Slot_" + slot, item);
+    }
+
+    public void AddItem(string slot, Item item) {
+        itemList.Add(slot, item);
     }
 }
