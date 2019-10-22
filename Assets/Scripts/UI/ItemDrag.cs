5538e421a452d8934158f81efc3772e0db2a40b3
Ahmad Sbeiti <ahmadsbeiti@outlook.com>
2019-10-22 08:59:19 -0400

    sellgrid, purchase button, etc. all updated and finished.

--- Assets/Scripts/UI/ItemDrag.cs
+++ Assets/Scripts/UI/ItemDrag.cs
@@ -4,50 +4,55 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ItemDrag : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandler
-{
+public class ItemDrag : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler {
     GameObject copie;
     Vector3 initialPos;
     public static GameObject itemStart;
     //Transform startParent;
-    public Color purchesedColor;
+    public Color purchasedColor;
     public Color normalColor;
     Sprite startSprite;
-    [HideInInspector]public bool spriteChange;
+    [HideInInspector] public bool spriteChange;
 
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
+    public void OnBeginDrag(PointerEventData eventData) {
         copie = gameObject;
         itemStart = gameObject;
         initialPos = transform.position;
         //startParent = transform.parent;
         startSprite = GetComponent<Image>().sprite;
-        spriteChange=false;
+        spriteChange = false;
         GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position = eventData.position;
-        if (spriteChange/*transform.parent!=startParent*/)
-            
-            transform.position = initialPos;
-
+    public void OnDrag(PointerEventData eventData) {
+        if (!StoreManager.Instance.storeActive) {
+            transform.position = eventData.position;
+            if (spriteChange)
+                transform.position = initialPos;
+        }
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
+    public void OnEndDrag(PointerEventData eventData) {
         itemStart = null;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
-        if (spriteChange/*transform.parent == startParent*/)
-        {
-            Item it=StoreManager.Instance.itemList[transform.parent.name];
-            transform.GetChild(0).GetComponent<Text>().text = it.Part.ToString();
-            transform.GetChild(0).gameObject.SetActive(true);
+        if (spriteChange) {
+            if (InventoryManager.Instance.inventoryType == InventoryType.BODYPART) {
+                Item item = InventoryManager.Instance.itemList[transform.parent.name];
+                transform.GetChild(0).GetComponent<Text>().text = item.Part.ToString().Replace("BodyPart_", "");
+                transform.GetChild(0).gameObject.SetActive(true);
+                InventoryManager.Instance.AddItemToBodyPart(item.Part, item);
+            } else if (InventoryManager.Instance.inventoryType == InventoryType.SELLGRID) {
+                if (InventoryManager.Instance.itemList.ContainsKey(transform.parent.name)) {
+                    Item item = InventoryManager.Instance.itemList[transform.parent.name];
+                    InventoryManager.Instance.AddItemToSellGrid(InventoryManager.Instance.sellList.Count + 1, item);
+                    InventoryManager.Instance.energySellAmount += item.Cost;
+                    UILinks.instance.sellAmount.text = "Energy: " + InventoryManager.Instance.energySellAmount;
+                    InventoryManager.Instance.itemList.Remove(transform.parent.name);
+                } else {
+                    UILinks.instance.errorMessage.text = "Item is already in the sell grid!";
+                }
+            }
         }
         transform.position = initialPos;
-
-        //Destroy(copie);
     }
 }
--- Assets/Scripts/UI/StoreController.cs
+++ Assets/Scripts/UI/StoreController.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class StoreController : MonoBehaviour {
 
     private Item clickedItem;
+    private string clickedSlot;
 
     public void Switch() {
         string text = UILinks.instance.goTo.text;
@@ -19,45 +22,101 @@ public class StoreController : MonoBehaviour {
     }
 
     public void OpenDetail(string slot) {
-        if (UIManager.Instance.storeActive) {
+        if (StoreManager.Instance.storeActive) {
             UILinks.instance.descriptionPanel.SetActive(true);
             Item item = StoreManager.Instance.itemList[slot];
             UILinks.instance.itemTitle.text = item.name;
             UILinks.instance.description.text = item.Description;
             UILinks.instance.cost.text = "Cost: " + item.Cost;
             clickedItem = item;
+            clickedSlot = slot;
         }
     }
 
     public void PurchaseItem() {
         if (clickedItem.Cost <= PlayerManager.Instance.player.stats.currentEnegy) {
-            PlayerManager.Instance.player.stats.currentEnegy -= clickedItem.Cost;
-            InventoryManager.Instance.AddItem(InventoryManager.Instance.itemList.Count + 1, clickedItem);
-        } else {
-
+            if (!InventoryManager.Instance.itemList.ContainsValue(clickedItem)) {
+                PlayerManager.Instance.player.stats.currentEnegy -= clickedItem.Cost;
+                InventoryManager.Instance.AddItem(InventoryManager.Instance.itemList.Count + 1, clickedItem);
+                GameObject.Find(clickedSlot + "_Image").transform.GetChild(0).gameObject.SetActive(true);
+            } else {
+                UILinks.instance.errorMessage.text = "You already purchased this item!";
+                UILinks.instance.errorMessage.gameObject.SetActive(true);
+                UIManager.Instance.isErrorMessageActive = true;
+            }
         }
+    }
+
+    public void RemoveAbilityFromBodyPart() {
+        Sprite sprite = Resources.Load<Sprite>("External/Sprites/PNG/Icons/Crystal_Icon");
+
+        BodyPart part = (BodyPart)Enum.Parse(typeof(BodyPart), EventSystem.current.currentSelectedGameObject.name);
+        InventoryManager.Instance.bodyPartList.Remove(part);
+        EventSystem.current.currentSelectedGameObject.transform.GetChild(1).GetComponent<Image>().sprite = sprite;
+
+        //temporary fix, figure out a way to fix this another way.... makes the inventory slots reset because without you need to click go to store and back hm weird
+        StoreManager.Instance.SetupItemInStoreBoard();
+        SetupItemInInventoryBoard();
+    }
+
+    public void SellItemsInSellGrid() {
+        Sprite sprite = Resources.Load<Sprite>("External/Sprites/PNG/Icons/Crystal_Icon");
+        for (int i = 0; i < 9; i++) {
+            GameObject slotImage = GameObject.Find("Sell_Slot_" + (i + 1)).transform.GetChild(0).gameObject;
+            slotImage.GetComponent<Image>().sprite = sprite;
+        }
+        InventoryManager.Instance.sellList.Clear();
+        PlayerManager.Instance.player.stats.currentEnegy += InventoryManager.Instance.energySellAmount;
+        InventoryManager.Instance.energySellAmount = 0;
+        UILinks.instance.sellAmount.text = "Energy: ";
+
+        //temporary fix, figure out a way to fix this another way...
+        SwitchInventory();
+        SwitchInventory();
+        SetupItemInInventoryBoard();
     }
 
     public void SwitchInventory() {
         if (UILinks.instance.bodyPartPanel.activeInHierarchy) {
             UILinks.instance.bodyPartPanel.SetActive(false);
             UILinks.instance.sellGrid.SetActive(true);
+            InventoryManager.Instance.inventoryType = InventoryType.SELLGRID;
         } else if (UILinks.instance.sellGrid.activeInHierarchy) {
             UILinks.instance.bodyPartPanel.SetActive(true);
             UILinks.instance.sellGrid.SetActive(false);
+            InventoryManager.Instance.inventoryType = InventoryType.BODYPART;
         }
     }
 
     private void OpenShop() {
         UILinks.instance.title.text = "Store";
         UILinks.instance.goTo.text = "Go To Inventory";
-        UIManager.Instance.storeActive = true;
     }
 
     private void CloseShop() {
         UILinks.instance.descriptionPanel.SetActive(false);
         UILinks.instance.switchInventoryType.SetActive(false);
-        UIManager.Instance.storeActive = false;
+        StoreManager.Instance.storeActive = false;
+        SetupItemInInventoryBoard();
+    }
+
+    private void SetupItemInInventoryBoard() {
+        Sprite sprite = Resources.Load<Sprite>("External/Sprites/PNG/Icons/Crystal_Icon");
+        for (int i = 0; i < 9; i++) {
+            GameObject slotImage = GameObject.Find("Slot_" + (i + 1) + "_Image");
+            if (InventoryManager.Instance.itemList.ContainsKey("Slot_" + (i + 1))) {
+                Item item = InventoryManager.Instance.itemList["Slot_" + (i + 1)];
+                slotImage.GetComponentInChildren<Image>().sprite = item.imgSprite;
+                slotImage.transform.GetChild(0).gameObject.SetActive(false);
+                if (InventoryManager.Instance.bodyPartList.ContainsValue(item)) {
+                    slotImage.transform.GetChild(0).GetComponent<Text>().text = item.Part.ToString().Replace("BodyPart_", "");
+                    slotImage.transform.GetChild(0).gameObject.SetActive(true);
+                }
+            } else {
+                slotImage.GetComponentInChildren<Image>().sprite = sprite;
+                slotImage.transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
     }
 
     private void OpenInventory() {
@@ -65,6 +124,7 @@ public class StoreController : MonoBehaviour {
         UILinks.instance.goTo.text = "Go To Store";
         UILinks.instance.switchInventoryType.SetActive(true);
         UILinks.instance.bodyPartPanel.SetActive(true);
+        InventoryManager.Instance.inventoryType = InventoryType.BODYPART;
     }
 
     private void CloseInventory() {
@@ -72,5 +132,7 @@ public class StoreController : MonoBehaviour {
         UILinks.instance.bodyPartPanel.SetActive(false);
         UILinks.instance.sellGrid.SetActive(false);
         UILinks.instance.switchInventoryType.SetActive(false);
+        StoreManager.Instance.storeActive = true;
+        StoreManager.Instance.SetupItemInStoreBoard();
     }
 }
