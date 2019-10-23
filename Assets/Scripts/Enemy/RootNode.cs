ceee8b7c6af1773de8c929463e4e87422de40b68
GabDesg <gabriel@khome.org>
2019-10-23 08:46:47 -0400

    Fixed RootNode trying to access when deleted

--- Assets/Scripts/Enemy/RootNode.cs
+++ Assets/Scripts/Enemy/RootNode.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RootNode  : MonoBehaviour
-{
-    public static readonly Vector2 timeToMakeNewNode = new Vector2(15f,35f);    
+public class RootNode : MonoBehaviour {
+    public static readonly Vector2 timeToMakeNewNode = new Vector2(15f, 35f);
     public static readonly float rootDistance = 6.5f;
     public static readonly float timePerNodeInSystem = 2.5f;          //Time to spawn new root increases by total nodes in system
 
@@ -13,28 +12,24 @@ public class RootNode  : MonoBehaviour
     float timeIncreaseForNextNode;
     float timeOfNextNode;
     RootNode[] neighborNodes;
-    int numOfNodes = 1; 
-    
-    public void Initialize(RootNode spawningRootNode, RootSystem _rootSystemParent)
-    {
+    int numOfNodes = 1;
+
+    public void Initialize(RootNode spawningRootNode, RootSystem _rootSystemParent) {
         neighborNodes = new RootNode[4] { spawningRootNode, null, null, null };
         timeOfNextNode = Time.time + Random.Range(timeToMakeNewNode.x, timeToMakeNewNode.y) * numOfNodes;
         rootSystemParent = _rootSystemParent;
         rootSystemParent.numberOfRoots++;
     }
 
-    public void DestroyRootSystem()
-    {
+    public void DestroyRootSystem() {
         for (int i = 1; i < 4; i++) //skip first one, since that is the parent
             if (neighborNodes[i])
                 neighborNodes[i].DestroyRootSystem();
         GameObject.Destroy(gameObject);
     }
 
-    public void RefreshRootNode()
-    {
-        if (numOfNodes < 4 && Time.time >= timeOfNextNode)
-        {
+    public void RefreshRootNode() {
+        if (numOfNodes < 4 && Time.time >= timeOfNextNode) {
             neighborNodes[numOfNodes] = SpawnNewRootNode(this);
             numOfNodes++;
             timeOfNextNode = Time.time + Random.Range(timeToMakeNewNode.x, timeToMakeNewNode.y) * numOfNodes + rootSystemParent.numberOfRoots * timePerNodeInSystem;
@@ -45,16 +40,15 @@ public class RootNode  : MonoBehaviour
                 neighborNodes[i].RefreshRootNode();
     }
 
-   
-    public RootNode SpawnNewRootNode(RootNode parentNode)
-    {
+
+    public RootNode SpawnNewRootNode(RootNode parentNode) {
 
 
-        Vector2 dir = Random.insideUnitCircle.normalized*rootDistance;
+        Vector2 dir = Random.insideUnitCircle.normalized * rootDistance;
         GameObject newRootNodeObj = GameObject.Instantiate(EnemyManager.rootPrefab);
         newRootNodeObj.transform.position = new Vector3(dir.x + parentNode.transform.position.x, 1, dir.y + parentNode.transform.position.z);
         newRootNodeObj.transform.SetParent(EnemyManager.Instance.rootNodeParent);
-        newRootNodeObj.GetComponent<LineRenderer>().SetPosition(0, new Vector3(newRootNodeObj.transform.position.x,1, newRootNodeObj.transform.position.z));
+        newRootNodeObj.GetComponent<LineRenderer>().SetPosition(0, new Vector3(newRootNodeObj.transform.position.x, 1, newRootNodeObj.transform.position.z));
         newRootNodeObj.GetComponent<LineRenderer>().SetPosition(1, new Vector3(parentNode.transform.position.x, 1, parentNode.transform.position.z));
         RootNode newRootNode = newRootNodeObj.GetComponent<RootNode>();
         newRootNode.Initialize(this, this.rootSystemParent);
