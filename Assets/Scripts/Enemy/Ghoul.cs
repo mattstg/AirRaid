ddcfd0b159b6d728ecd7c809b4e86425edf2e103
marcuslelus <44736661+marcuslelus@users.noreply.github.com>
2019-10-23 08:00:32 -0400

    final commit

--- Assets/Scripts/Enemy/Ghoul.cs
+++ Assets/Scripts/Enemy/Ghoul.cs
@@ -8,7 +8,7 @@ public class Ghoul : AnimatedEnemy
     Rigidbody rb;
     GameObject ghouleEvolution;
     //readonly float WALKING_SPEED = 6f;
-    //readonly float MIN_ENERGY_EVOLVE = 10f; //If dies, but has over 10 energy, should spawn a new Troll
+    readonly float MIN_ENERGY_EVOLVE = 10f; //If dies, but has over 10 energy, should spawn a new Troll
     //readonly float MAX_ENERGY_BEFORE_EVOLVE = 15f; //Should die and spawn a new Troll
     readonly float MAX_HP = 100f;
 
@@ -49,7 +49,10 @@ public class Ghoul : AnimatedEnemy
     {
         yield return new WaitForSeconds(time);
         //GameObject troll = GameObject.Instantiate<GameObject>(ghouleEvolution, transform.parent);
-        EnemyManager.Instance.SpawnEnemy(type, transform.position, 100);
+        if(energy > MIN_ENERGY_EVOLVE)
+        {
+            EnemyManager.Instance.SpawnEnemy(type, transform.position, 100);
+        }
         base.Die();
 
         //troll.transform.position = transform.position;
