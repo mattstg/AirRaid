aec25dfcbb2f06bd213a75ee1e5879e1f7239595
chimera279 <55556029+chimera279@users.noreply.github.com>
2019-11-01 07:29:06 -0400

    WORKING SHADER holy shit that was quick

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class Building : MonoBehaviour, IHittable
 {
-    public float hp;
+    public float hp, maxHP;
     public AudioSource audioObject;
 
     public void Initialize(float _hp)
     {
         hp = _hp;
+        maxHP = hp;
         audioObject = GameObject.Find("AudioObject").GetComponent<AudioSource>();
+    }
+
+    public void Refresh()
+    {
+        DoShader();
     }
 
     public void HitByProjectile(float damage)
@@ -26,5 +32,13 @@ public class Building : MonoBehaviour, IHittable
         audioObject.transform.position = this.transform.position;
         audioObject.Play();
         BuildingManager.Instance.BuildingDied(this);
+    }
+
+    public void DoShader()
+    {
+        Renderer r = this.GetComponent<Renderer>();
+        r.material.SetFloat("GlowEmissionMultiplier", (maxHP - hp) / maxHP * 30);
+        r.material.SetFloat("GlowColorIntensity", (maxHP - hp) / maxHP * 10);
+        r.material.SetFloat("GlowBaseFrequency", (maxHP -  hp) / maxHP * 5);
     }
 }
