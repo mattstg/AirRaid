63d2aa6f9b9697709f5341b25fe900039e295ba8
Ashwin <ashwinawsome@gmail.com>
2019-11-01 13:40:50 -0400

    Final Commit
    Added scripts and prefabs for Fighter and Bomber enemies.

---
+++ Assets/Scripts/Enemy/BomberEnemy.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberEnemy : Ship
+{
+    Transform player;
+    Rigidbody rb;
+    float speedOfBomber = 60f;
+    float countdown;
+    bool attackMode;
+    readonly float BOMB_LIFESPAN = 6;
+    readonly float BOMBER_ATTACK_SPEED = 10;
+    readonly float TURNING_RATE = 5;  //time to preform a full 360
+    [HideInInspector]public float distanceFromPlayer = 30;
+    [HideInInspector]public float distanceFromBuilding = 40;
+
+    public override void Initialize(float startingEnergy)
+    {
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
+        rb = GetComponent<Rigidbody>();
+        base.Initialize(startingEnergy);
+    }
+
+    public override void PhysicRefresh()
+    {
+        base.PhysicRefresh();
+        EvadePlayerAndTargetBuildings();
+    }
+
+    public override void Refresh()
+    {
+        base.Refresh();
+        
+    }
+
+    void EvadePlayerAndTargetBuildings()
+    {
+        //evade player
+        if (PlayerManager.Instance.player.isAlive && (transform.position - player.position).sqrMagnitude <= distanceFromPlayer* distanceFromBuilding)
+        {
+            attackMode = false;
+            Vector3 dir = (transform.position - player.position).normalized;
+            dir.y = 0;
+
+            transform.forward = Vector3.RotateTowards(transform.forward, dir, 2 * Mathf.PI / TURNING_RATE * Time.fixedDeltaTime, 0);
+            rb.velocity = transform.forward * speedOfBomber * Time.deltaTime;
+        }
+        else
+        {
+            Transform closestBuilding = ClosestBuilding();
+            Vector3 dirVector = (closestBuilding.position - transform.position);
+            dirVector.y = 0;
+
+            //target buildings to be implemented
+            if (dirVector.sqrMagnitude >= distanceFromBuilding * distanceFromBuilding)
+            {
+                transform.forward = Vector3.RotateTowards(transform.forward, dirVector.normalized, 2 * Mathf.PI / TURNING_RATE * Time.fixedDeltaTime, 0);
+                rb.velocity = transform.forward * speedOfBomber * Time.deltaTime;
+            }
+                
+            else
+            {
+                attackMode = true;
+                UpdateAttackMode();
+            }
+        }
+    }
+
+    private void UpdateAttackMode()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0)
+        {
+            if (!attackMode)
+                return; //Lost track of the player
+
+            Projectile p = BulletManager.Instance.CreateProjectile(ProjectileType.Bomb, 
+                                                    transform.position + new Vector3(0, -5f, 0), 
+                                                    -transform.up,
+                                                    rb.velocity,
+                                                    BOMB_LIFESPAN, 
+                                                    0);
+
+            Collider c = p.gameObject.GetComponentInChildren<Collider>();
+            Physics.IgnoreCollision(c, transform.GetComponent<Collider>());  //make bomb not collide with bomber
+            countdown = BOMBER_ATTACK_SPEED;
+        }
+    }
+
+    Transform ClosestBuilding()
+    {
+        Transform closestBuilding = null;
+
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+
+        foreach (Building building in BuildingManager.Instance.allBuildings)
+        {
+            Vector3 directionToTarget = building.gameObject.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                closestBuilding = building.gameObject.transform;
+            }
+        }
+        return closestBuilding;
+    }
+}
---
+++ Assets/Scripts/Enemy/FighterEnemy.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterEnemy : Ship
+{
+
+    Transform player;
+    Rigidbody rb;
+    float speedOfFighter = 85f;
+
+    GameObject randomBuilding;
+
+    float countdown;
+    bool attackMode;
+    readonly float MIN_ANGLE_FOR_ATTACK = 15;
+    readonly float FIGHTER_ATTACK_SPEED = 2f;
+    readonly float FIGHTER_ATTACK_VELO = 6f;
+    readonly float TURNING_RATE = 5;  //time to preform a full 360
+
+    public override void Initialize(float startingEnergy)
+    {
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
+        rb = gameObject.GetComponent<Rigidbody>();
+        base.Initialize(startingEnergy);
+        randomBuilding = BuildingManager.Instance.GetRandomBuilding().gameObject;
+    }
+
+    public override void Refresh()
+    {
+        base.Refresh();
+
+        if(PlayerManager.Instance.player.isAlive)
+        {
+            if (!attackMode)
+                UpdateFindPlayer();
+            else
+                UpdateAttackMode();
+        }
+    }
+
+    public override void PhysicRefresh()
+    {
+        base.PhysicRefresh();
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        Vector3 targetPos = (PlayerManager.Instance.player.isAlive ? player.position : (randomBuilding = randomBuilding ?? BuildingManager.Instance.GetRandomBuilding().gameObject).transform.position);
+        transform.forward = Vector3.RotateTowards(transform.forward, (targetPos - transform.position).normalized, 2 * Mathf.PI / TURNING_RATE * Time.fixedDeltaTime, 0);
+        rb.velocity = transform.forward * speedOfFighter * Time.deltaTime;
+    }
+
+    private void UpdateFindPlayer()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown < 0)
+        {
+            countdown = 3;
+            //Check if player is in sights
+            if (Mathf.Abs(Vector3.SignedAngle(transform.position, player.position, transform.right)) < MIN_ANGLE_FOR_ATTACK)
+            {
+                attackMode = true;
+                countdown = FIGHTER_ATTACK_SPEED;
+            }
+        }
+    }
+
+    private void UpdateAttackMode()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0)
+        {
+            if (!attackMode)
+                return; //Lost track of the player
+
+            BulletManager.Instance.CreateProjectile(ProjectileType.BasicBullet, transform.position + new Vector3(0, 0, transform.localScale.z/2), (player.position - transform.position).normalized, rb.velocity, 60, FIGHTER_ATTACK_VELO);
+            countdown = FIGHTER_ATTACK_SPEED;
+        }
+    }
+}    
+
